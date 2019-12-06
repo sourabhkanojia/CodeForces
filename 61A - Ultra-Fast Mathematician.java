4f42ac1dd73c93ab4ea5b2a061b9b49b2979ed34
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2019-12-07 01:57:50 +0530

    Update 61A - Ultra-Fast Mathematician.java

--- 61A - Ultra-Fast Mathematician.java
+++ 61A - Ultra-Fast Mathematician.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+imort java.util.Scanner;
 
 
 public class Candidate {
