d853e0bab6bd2353557e274f2dfc743b52d849ab
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2019-12-07 01:56:56 +0530

    Create 61A - Ultra-Fast Mathematician.java

---
+++ 61A - Ultra-Fast Mathematician.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Candidate {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+
+        char[] c=new char[a.length()];
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='0'){
+                if(b.charAt(i)=='1'){
+                    c[i]='1';
+                }
+                else{
+                    c[i]='0';
+                }
+            }
+            else{
+                if(b.charAt(i)=='0'){
+                    c[i]='1';
+                }
+                else{
+                    c[i]='0';
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
