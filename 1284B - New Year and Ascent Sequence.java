c028cbbfa8462195fb255267ae1abc8df3f400d4
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-01-05 01:43:08 +0530

    Update 1284B - New Year and Ascent Sequence.java

--- 1284B - New Year and Ascent Sequence.java
+++ 1284B - New Year and Ascent Sequence.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-public class test {
+public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n = sc.nextInt();
