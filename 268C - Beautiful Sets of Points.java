937c193fb3f5787ce5f90493161877aaac835683
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-01-13 00:29:31 +0530

    Update 268C - Beautiful Sets of Points.java

--- 268C - Beautiful Sets of Points.java
+++ 268C - Beautiful Sets of Points.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(),m=sc.nextInt();
