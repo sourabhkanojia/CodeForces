4509ef272b7e945afaea5541fcc32f12ff6ae979
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-01-24 19:37:49 +0530

    Update 217A - Ice Skating.java

--- 217A - Ice Skating.java
+++ 217A - Ice Skating.java
@@ -3,31 +3,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
- 
+
 public class Main {
-    public static void main(String agr[]) {
+    static int mod = (int) 1e9 + 7;
+    static boolean[] vis;
+    public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        Point p[]=new Point[n];
-        boolean vis[]=new boolean[n];
-        for (int i=0;i<n;i++){
-            p[i]=new Point(sc.nextInt(),sc.nextInt());
-        }
-        int cnt=0;
-        for (int i=0;i<n;i++){
-            if (!vis[i]){
-                cnt++;
-                dfs(p,i,vis);
+        Point[] p=new Point[n];
+        for(int i=0;i<n;i++)p[i]=new Point(sc.nextInt(),sc.nextInt());
+        vis=new boolean[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,n,p);
+                count++;
             }
         }
-        System.out.println(cnt-1);
+        System.out.println(count-1);
     }
-    static void dfs(Point p[],int v,boolean vis[]){
-        if (vis[v])return;
-        vis[v]=true;
-        for (int i=0;i<p.length;i++){
-            if (p[i].x==p[v].x || p[i].y==p[v].y){
-                dfs(p,i,vis);
+    static void dfs(int i,int n,Point[] p){
+        vis[i]=true;
+        for(int j=0;j<n;j++){
+            if(!vis[j]){
+                if(p[i].x==p[j].x || p[i].y==p[j].y){
+                    dfs(j,n,p);
+                }
             }
         }
     }
