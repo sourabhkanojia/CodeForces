a545584aa6dbe7a7d46c78cca586edba04c4a5aa
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-01-27 02:30:38 +0530

    Update 245H - Queries for Number of Palindromes.java

--- 245H - Queries for Number of Palindromes.java
+++ 245H - Queries for Number of Palindromes.java
@@ -1,108 +1,43 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
-import java.io.*;
 
-public class problems {
-    public static void main(String args[] ) throws Exception {
-        FastReader sc=new FastReader();
+public class Main {
+    static int mod = (int) 1e9 + 7;
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         int n=s.length();
         s=" "+s;
-        int[][] dp=new int[5001][5001];
-        int[][] pal=new int[5001][5001];
-        // Length of 1
-        for(int i=1;i<=n;i++){
-            pal[i][i]=1;
-        }
+        int[][] dp=new int[n+1][n+1];
 
-        // Length of 2
+        for(int i=1;i<=n;i++)dp[i][i]=1;
         for(int i=1;i<n;i++){
-            if(s.charAt(i)== s.charAt(i+1)){
-                pal[i][i+1]=1;
-            }
+            if(s.charAt(i)==s.charAt(i+1))dp[i][i+1]=1;
         }
-
         for(int len=0;len<n-2;len++){
-            //i is a starting index
             for(int i=1;i<n-len;i++){
-                //if we have a palindrome ans the same letter on both sides of the palindrome
-                if(pal[i][i+len]==1 && s.charAt(i-1)== s.charAt(i+len+1)){
-                    pal[i-1][i+len+1]=1;
+                if(dp[i][i+len]==1 && s.charAt(i-1)==s.charAt(i+len+1)){
+                    dp[i-1][i+len+1]=1;
                 }
             }
         }
-
-        // Prefix Sum 2-d
-        for (int i=1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = pal[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
-            }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++)dp[i][j]+=dp[i][j-1];
         }
+        for(int j=1;j<=n;j++){
+            for(int i=1;i<=n;i++)dp[i][j]+=dp[i-1][j];
+        }
+
         StringBuilder sb=new StringBuilder();
         int q=sc.nextInt();
         while(q-->0){
-            int x=sc.nextInt();
-            int y=sc.nextInt();
-            int ans=0;
-
-            ans=dp[y][y] - dp[x-1][y] - dp[y][x-1] + dp[x-1][x-1];
-            sb.append(ans+"\n");
+            int a=sc.nextInt(),b=sc.nextInt();
+            sb.append(dp[b][b]-dp[b][a-1]-dp[a-1][b]+dp[a-1][a-1]+"\n");
         }
-        System.out.print(sb);
-    }
-}
-class FastReader
-{
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastReader()
-    {
-        br = new BufferedReader(new
-                InputStreamReader(System.in));
-    }
-
-    String next()
-    {
-        while (st == null || !st.hasMoreElements())
-        {
-            try
-            {
-                st = new StringTokenizer(br.readLine());
-            }
-            catch (IOException  e)
-            {
-                e.printStackTrace();
-            }
-        }
-        return st.nextToken();
-    }
-
-    int nextInt()
-    {
-        return Integer.parseInt(next());
-    }
-
-    long nextLong()
-    {
-        return Long.parseLong(next());
-    }
-
-    double nextDouble()
-    {
-        return Double.parseDouble(next());
-    }
-
-    String nextLine()
-    {
-        String str = "";
-        try
-        {
-            str = br.readLine();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return str;
+        System.out.println(sb);
     }
 }
