3fd6a86945910e4af4378d9b9e9f91e7468730dd
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-03-22 02:10:51 +0530

    Create 735D - Taxes.java

---
+++ 735D - Taxes.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder();
+        int n=sc.nextInt();
+        if(n%2==0){
+            if(n==2) System.out.println(1);
+            else System.out.println(2);
+        }
+        else{
+            if(isPrime(n)) System.out.println(1);
+            else if(isPrime(n-2)) System.out.println(2);
+            else System.out.println(3);
+        }
+    }
+    static boolean isPrime(int n){
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0 || n%(n/i)==0)return false;
+        }
+        return true;
+    }
+}
