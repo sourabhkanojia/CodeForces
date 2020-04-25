65b84170e78d752450dbe0faf2a194f0aa75b558
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-04-26 02:54:14 +0530

    Update 522B - Photo to Remember.java

--- 522B - Photo to Remember.java
+++ 522B - Photo to Remember.java
@@ -8,25 +8,25 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int n=sc.nextInt();
         Point[] p=new Point[n];
-        int sum=0;
-        TreeSet<Integer> treeSet=new TreeSet<>();
-        int[] heights=new int[1001];
+        int sum=0,max=0,max2=0;
         for(int i=0;i<n;i++){
             p[i]=new Point(sc.nextInt(),sc.nextInt());
             sum+=p[i].x;
-            treeSet.add(p[i].y);
-            heights[p[i].y]++;
+            max=Math.max(max,p[i].y);
         }
+        int count=0;
         for(int i=0;i<n;i++){
-            if(heights[p[i].y]>1){
-                sb.append((sum-p[i].x)*treeSet.last());
+            if(max!=p[i].y){
+                max2=Math.max(max2,p[i].y);
             }
-            else{
-                if(treeSet.higher(p[i].y)==null){
-                    sb.append((sum-p[i].x)*treeSet.lower(p[i].y));
-                }
-                else sb.append((sum-p[i].x)*treeSet.last());
+            else count++;
+        }
+        if(count>1)max2=max;
+        for(int i=0;i<n;i++){
+            if(p[i].y==max){
+                sb.append((sum-p[i].x)*max2);
             }
+            else sb.append((sum-p[i].x)*max);
             sb.append(" ");
         }
         System.out.println(sb);
