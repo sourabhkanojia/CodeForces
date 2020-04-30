bd24e07672044b29df5fa553251201c561c8f2dc
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-05-01 01:11:22 +0530

    Create 1342A - Road To Zero.java

---
+++ 1342A - Road To Zero.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FastReader sc = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        int t=sc.nextInt();
+        while(t-->0){
+            long x=sc.nextInt(),y=sc.nextInt(),a=sc.nextInt(),b=sc.nextInt();
+            long count=0;
+            if(2*a>b){
+                long min=Math.min(x,y);
+                x-=min;
+                y-=min;
+                count+=min;
+            }
+            count=count*b;
+            count+=(x+y)*a;
+            sb.append(count+"\n");
+        }
+        System.out.println(sb);
+    }
+}
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt() { return Integer.parseInt(next()); }
+    long nextLong() { return Long.parseLong(next()); }
+    double nextDouble() { return Double.parseDouble(next()); }
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
