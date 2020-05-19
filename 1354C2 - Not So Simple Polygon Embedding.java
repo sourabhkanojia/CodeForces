882258318e2368f4c6ff41e84f520d07fb788ba8
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-05-19 23:31:38 +0530

    Create 1354C2 - Not So Simple Polygon Embedding.java

---
+++ 1354C2 - Not So Simple Polygon Embedding.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FastReader sc = new FastReader();
+        StringBuilder sb=new StringBuilder();
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            double angle=Math.toRadians(360.0/(4*n));
+            double sin=Math.sin(angle);
+            double r=1/(2*sin);
+            double rotatedAngle=Math.toRadians(360.0/(8*n));
+            double cos=Math.cos(rotatedAngle);
+            double h=r*cos;
+            sb.append(2*h+"\n");
+        }
+        System.out.println(sb);
+    }
+}
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt() { return Integer.parseInt(next()); }
+    long nextLong() { return Long.parseLong(next()); }
+    double nextDouble() { return Double.parseDouble(next()); }
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
